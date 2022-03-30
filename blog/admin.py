63af1dd7a7e81b07168cd7f1559bc6b5f5e995b1
Kian <sabbah128@gmail.com>
2022-03-30 12:30:58 -0700

    use ForeignKey for author

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_filter = ('status',)
-    list_display = ('title', 'counted_view', 'status','published_date', 'created_date',)
+    list_filter = ('status','author',)
+    list_display = ('title','author', 'counted_view', 'status','published_date', 'created_date',)
     # ordering = ('-created_date',)
     search_fields = ('title', 'content',)
 
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    # author
     # image    
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     # category
