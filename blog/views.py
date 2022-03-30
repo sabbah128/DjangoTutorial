ee65d80f5c670a6124c5023130ede1145617d9a4
Kian <sabbah128@gmail.com>
2022-03-30 07:36:17 -0700

    optimal models for sent to html

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 
-content = {'name': 'Hossein', 'lname': 'KianAra', 'job': 'ML & BackEnd'}
 def blog_view(request):
-    return render(request, 'blog/blog-home.html', content)
+    posts = Post.objects.filter(status=1)
+    return render(request, 'blog/blog-home.html', {'posts': posts})
 
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
-    
+def blog_single(request, pid):
+    post = get_object_or_404(Post, pk=pid, status=1)
+    return render(request, 'blog/blog-single.html', {'post': post})
