b118fbaab0493666afc7a3009a7432b84f2cc6e6
Kian <sabbah128@gmail.com>
2022-04-01 12:42:54 -0700

    search query

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,15 @@ def blog_single(request, pid):
     post = get_object_or_404(Post, pk=pid, status=1)
     return render(request, 'blog/blog-single.html', {'post': post})
 
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s := request.GET.get('s'): # py 3.8 walrus
+            posts = posts.filter(content__contains=s)
+    return render(request, 'blog/blog-home.html', {'posts': posts})
+
+
 # def blog_category(request, cat_name):
 #     posts = Post.objects.filter(status=1)
 #     posts = posts.filter(category__name=cat_name)
